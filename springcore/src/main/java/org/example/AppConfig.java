2bb4ce8289a7759672de3d5e0128e36fd46a9f19
alagurasu <alagurasumca1992@gmail.com>
2025-07-27 23:28:15 +0800

    Approach 3 correction: In the @ComponentScan annotation basepackage configuration not required as we are defining the beans in the same class.

--- springcore/src/main/java/org/example/AppConfig.java
+++ springcore/src/main/java/org/example/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Map;
 
 @Configuration
-@ComponentScan(basePackages = "com.example")
+@ComponentScan
 public class AppConfig {
 
     @Bean
